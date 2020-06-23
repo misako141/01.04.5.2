b53ca134bc472e4bec6373e7375c1b602128117d
misako141 <65210068+misako141@users.noreply.github.com>
2020-06-24 01:29:01 +0300

    Add files via upload

---
+++ Курилова_19-11-1 ИСиП_МДК 01.04_5.2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <time.h> 
+
+using namespace std;
+
+int random(int b){
+int rn = (rand() % b);
+return rn;	
+}
+int main()
+{
+	setlocale(0,"");
+srand((time(NULL)));
+char *ar[51] = {"à", "á", "â", "ã", "ä", "å", "¸", "æ", "ç", "è", "é", "ê", "ë", "ì", "í", "î", "ï", "ð", "ñ",
+        "ò", "ó", "ô", "õ", "ö", "÷", "ø", "ù", "ú", "û", "ü", "ý", "þ", "ÿ", "!", ".", ",", "?",":","*","(",")","+","=","_","-","%","#","/","¹",";","@"};
+	string a = "";
+	
+	int ars = 51;
+
+	for(int i=0;i<6;i++){
+		
+		int rand = random(ars);
+		a += ar[rand];
+	}
+	cout << a << endl;
+	
+return 0;
+}
